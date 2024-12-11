8e290fcd496c98fb3d1f05e9ba522fd9b2d97a80
Talleres Puente Alto <tallerespa@inacapmail.cl>
2024-12-11 19:55:09 -0300

    RegisterAlum

---
+++ Alumnos/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Alumnos.AppMovil;
+using Foundation;
+
+namespace Alumnos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
